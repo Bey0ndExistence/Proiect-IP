cf6c50735d9cdc63052a954e5f37f586292c8246
GalatanuM <101355903+GalatanuM@users.noreply.github.com>
2024-05-19 17:20:12 +0300

    Update interfata
    -login
    -register
    -active user list
    -chat

---
+++ ChatApp/RegisterControl.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace ChatApp
+{
+    partial class RegisterControl
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxConfirmPassword;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxConfirmEmail;
+        private System.Windows.Forms.TextBox textBoxFirstname;
+        private System.Windows.Forms.TextBox textBoxLastname;
+        private System.Windows.Forms.TextBox textBoxPhoneNumber;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelConfirmPassword;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelConfirmEmail;
+        private System.Windows.Forms.Label labelFirstname;
+        private System.Windows.Forms.Label labelLastname;
+        private System.Windows.Forms.Label labelPhoneNumber;
+        private System.Windows.Forms.Button fromRegisterGoBackButton;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxConfirmPassword = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxConfirmEmail = new System.Windows.Forms.TextBox();
+            this.textBoxFirstname = new System.Windows.Forms.TextBox();
+            this.textBoxLastname = new System.Windows.Forms.TextBox();
+            this.textBoxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelConfirmPassword = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelConfirmEmail = new System.Windows.Forms.Label();
+            this.labelFirstname = new System.Windows.Forms.Label();
+            this.labelLastname = new System.Windows.Forms.Label();
+            this.labelPhoneNumber = new System.Windows.Forms.Label();
+            this.fromRegisterGoBackButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(350, 50);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(350, 90);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // textBoxConfirmPassword
+            // 
+            this.textBoxConfirmPassword.Location = new System.Drawing.Point(350, 130);
+            this.textBoxConfirmPassword.Name = "textBoxConfirmPassword";
+            this.textBoxConfirmPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxConfirmPassword.TabIndex = 2;
+            this.textBoxConfirmPassword.UseSystemPasswordChar = true;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(350, 170);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEmail.TabIndex = 3;
+            // 
+            // textBoxConfirmEmail
+            // 
+            this.textBoxConfirmEmail.Location = new System.Drawing.Point(350, 210);
+            this.textBoxConfirmEmail.Name = "textBoxConfirmEmail";
+            this.textBoxConfirmEmail.Size = new System.Drawing.Size(100, 20);
+            this.textBoxConfirmEmail.TabIndex = 4;
+            // 
+            // textBoxFirstname
+            // 
+            this.textBoxFirstname.Location = new System.Drawing.Point(350, 250);
+            this.textBoxFirstname.Name = "textBoxFirstname";
+            this.textBoxFirstname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFirstname.TabIndex = 5;
+            // 
+            // textBoxLastname
+            // 
+            this.textBoxLastname.Location = new System.Drawing.Point(350, 290);
+            this.textBoxLastname.Name = "textBoxLastname";
+            this.textBoxLastname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLastname.TabIndex = 6;
+            // 
+            // textBoxPhoneNumber
+            // 
+            this.textBoxPhoneNumber.Location = new System.Drawing.Point(350, 330);
+            this.textBoxPhoneNumber.Name = "textBoxPhoneNumber";
+            this.textBoxPhoneNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPhoneNumber.TabIndex = 7;
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(350, 370);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(100, 23);
+            this.registerButton.TabIndex = 8;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(260, 50);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(58, 13);
+            this.labelUsername.TabIndex = 10;
+            this.labelUsername.Text = "Username:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(260, 90);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(56, 13);
+            this.labelPassword.TabIndex = 11;
+            this.labelPassword.Text = "Password:";
+            // 
+            // labelConfirmPassword
+            // 
+            this.labelConfirmPassword.AutoSize = true;
+            this.labelConfirmPassword.Location = new System.Drawing.Point(230, 130);
+            this.labelConfirmPassword.Name = "labelConfirmPassword";
+            this.labelConfirmPassword.Size = new System.Drawing.Size(94, 13);
+            this.labelConfirmPassword.TabIndex = 12;
+            this.labelConfirmPassword.Text = "Confirm Password:";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(283, 170);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(35, 13);
+            this.labelEmail.TabIndex = 13;
+            this.labelEmail.Text = "Email:";
+            // 
+            // labelConfirmEmail
+            // 
+            this.labelConfirmEmail.AutoSize = true;
+            this.labelConfirmEmail.Location = new System.Drawing.Point(230, 210);
+            this.labelConfirmEmail.Name = "labelConfirmEmail";
+            this.labelConfirmEmail.Size = new System.Drawing.Size(73, 13);
+            this.labelConfirmEmail.TabIndex = 14;
+            this.labelConfirmEmail.Text = "Confirm Email:";
+            // 
+            // labelFirstname
+            // 
+            this.labelFirstname.AutoSize = true;
+            this.labelFirstname.Location = new System.Drawing.Point(269, 250);
+            this.labelFirstname.Name = "labelFirstname";
+            this.labelFirstname.Size = new System.Drawing.Size(49, 13);
+            this.labelFirstname.TabIndex = 15;
+            this.labelFirstname.Text = "Firstame:";
+            // 
+            // labelLastname
+            // 
+            this.labelLastname.AutoSize = true;
+            this.labelLastname.Location = new System.Drawing.Point(269, 290);
+            this.labelLastname.Name = "labelLastname";
+            this.labelLastname.Size = new System.Drawing.Size(56, 13);
+            this.labelLastname.TabIndex = 16;
+            this.labelLastname.Text = "Lastname:";
+            // 
+            // labelPhoneNumber
+            // 
+            this.labelPhoneNumber.AutoSize = true;
+            this.labelPhoneNumber.Location = new System.Drawing.Point(237, 330);
+            this.labelPhoneNumber.Name = "labelPhoneNumber";
+            this.labelPhoneNumber.Size = new System.Drawing.Size(81, 13);
+            this.labelPhoneNumber.TabIndex = 17;
+            this.labelPhoneNumber.Text = "Phone Number:";
+            // 
+            // fromRegisterGoBackButton
+            // 
+            this.fromRegisterGoBackButton.Location = new System.Drawing.Point(697, 424);
+            this.fromRegisterGoBackButton.Name = "fromRegisterGoBackButton";
+            this.fromRegisterGoBackButton.Size = new System.Drawing.Size(100, 23);
+            this.fromRegisterGoBackButton.TabIndex = 18;
+            this.fromRegisterGoBackButton.Text = "Back";
+            this.fromRegisterGoBackButton.UseVisualStyleBackColor = true;
+            this.fromRegisterGoBackButton.Click += new System.EventHandler(this.switchToLoginButton_Click);
+            // 
+            // RegisterControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.fromRegisterGoBackButton);
+            this.Controls.Add(this.labelPhoneNumber);
+            this.Controls.Add(this.labelLastname);
+            this.Controls.Add(this.labelFirstname);
+            this.Controls.Add(this.labelConfirmEmail);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelConfirmPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.textBoxPhoneNumber);
+            this.Controls.Add(this.textBoxLastname);
+            this.Controls.Add(this.textBoxFirstname);
+            this.Controls.Add(this.textBoxConfirmEmail);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxConfirmPassword);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Name = "RegisterControl";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
