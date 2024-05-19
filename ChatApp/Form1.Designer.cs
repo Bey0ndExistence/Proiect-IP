4e6acee03ca66ac3e4ef34b126159597ac243898
GalatanuM <101355903+GalatanuM@users.noreply.github.com>
2024-05-19 18:57:00 +0300

    Update interfata

---
+++ ChatApp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ChatApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private LoginControl loginControl;
+        private RegisterControl registerControl;
+        private ChatControl chatControl;
+        private ActiveUserControl activeUsersControl;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.loginControl = new ChatApp.LoginControl();
+            this.registerControl = new ChatApp.RegisterControl();
+            this.chatControl = new ChatApp.ChatControl();
+            this.activeUsersControl = new ChatApp.ActiveUserControl(); // Add this line
+            this.SuspendLayout();
+            // 
+            // loginControl
+            // 
+            this.loginControl.Location = new System.Drawing.Point(0, 0);
+            this.loginControl.Name = "loginControl";
+            this.loginControl.Size = new System.Drawing.Size(800, 450);
+            this.loginControl.TabIndex = 0;
+            // 
+            // registerControl
+            // 
+            this.registerControl.Location = new System.Drawing.Point(0, 0);
+            this.registerControl.Name = "registerControl";
+            this.registerControl.Size = new System.Drawing.Size(800, 450);
+            this.registerControl.TabIndex = 1;
+            this.registerControl.Visible = false;
+            // 
+            // chatControl
+            // 
+            this.chatControl.Location = new System.Drawing.Point(0, 0);
+            this.chatControl.Name = "chatControl";
+            this.chatControl.Size = new System.Drawing.Size(800, 450);
+            this.chatControl.TabIndex = 2;
+            this.chatControl.Visible = false;
+            // 
+            // activeUsersControl
+            // 
+            this.activeUsersControl.Location = new System.Drawing.Point(0, 0);
+            this.activeUsersControl.Name = "activeUsersControl";
+            this.activeUsersControl.Size = new System.Drawing.Size(800, 450);
+            this.activeUsersControl.TabIndex = 3;
+            this.activeUsersControl.Visible = false; // Set visible to false initially
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginControl);
+            this.Controls.Add(this.registerControl);
+            this.Controls.Add(this.chatControl);
+            this.Controls.Add(this.activeUsersControl); // Add activeUsersControl to the controls collection
+            this.Name = "Form1";
+            this.Text = "Chat Application";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ ChatApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ChatApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Attach event handlers to the user controls
+            loginControl.LoginSuccessful += SwitchToActiveUsers;
+            loginControl.SwitchToRegister += SwitchToRegister;
+            registerControl.SwitchToLogin += SwitchToLogin;
+            activeUsersControl.SwitchToLogin += SwitchToLogin;
+            activeUsersControl.SwitchToChat += SwitchToChat;
+            chatControl.SwitchToActiveUsers += SwitchToActiveUsers;
+        }
+
+        private void SwitchToActiveUsers(object sender, EventArgs e)
+        {
+            // Show the active users control and hide all the other controls
+            loginControl.Visible = false;
+            activeUsersControl.Visible = true;
+            registerControl.Visible = false;
+            chatControl.Visible = false;
+        }
+
+        private void SwitchToRegister(object sender, EventArgs e)
+        {
+            // Show the register control and hide all the other controls
+            loginControl.Visible = false;
+            activeUsersControl.Visible = false;
+            registerControl.Visible = true;
+            chatControl.Visible = false;
+            registerControl.ResetFields();
+        }
+
+        private void SwitchToLogin(object sender, EventArgs e)
+        {
+            // Show the login control and hide all the other controls
+            loginControl.Visible = true;
+            activeUsersControl.Visible = false;
+            registerControl.Visible = false;
+            chatControl.Visible = false;
+            loginControl.ResetFields();
+        }
+        private void SwitchToChat(object sender, EventArgs e)
+        {
+            // Show the chat control and hide all the other controls
+            loginControl.Visible = false;
+            activeUsersControl.Visible = false;
+            registerControl.Visible = false;
+            chatControl.Visible = true;
+            chatControl.ResetFields();
+        }
+    }
+}
